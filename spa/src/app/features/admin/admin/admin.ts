c30aacd9d6209254edbe68920cccb8409c95ee6e
jose23021995 <80795569+jose23021995@users.noreply.github.com>
2026-05-05 16:19:12 -0600

    estilos adaptados

--- spa/src/app/features/admin/admin/admin.ts
+++ spa/src/app/features/admin/admin/admin.ts
@@ -1,8 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardModule } from 'primeng/card';
 import { HeadersComponent } from '../../../shared/components/headers/headers';
-
+import texto  from '../../../shared/interfaces/text/admin-text.json';
+import {BannerInterface}  from '../../../shared/interfaces/models/banner.model';
+const { banner }=texto;
 @Component({
   selector: 'app-admin',
   standalone: true,
@@ -10,8 +12,17 @@ import { HeadersComponent } from '../../../shared/components/headers/headers';
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss'
 })
-export class AdminComponent {
+export class AdminComponent implements OnInit {
   // Signal para el título dinámico
-  public title = signal('Panel de Administración');
-  public subtitle = signal('Bienvenido, José Armando');
+  public bannerBody = signal(<BannerInterface>{});
+  public title = signal("");
+  public img = signal("");
+  
+  ngOnInit(): void {
+    this.bannerBody=signal(banner[0]);
+    const {image,title}=this.bannerBody();
+    this.title.set(title);
+    this.img.set(image);
+  }
+  
 }
--- spa/src/app/shared/components/headers/headers.ts
+++ spa/src/app/shared/components/headers/headers.ts
@@ -1,6 +1,6 @@
-import { Component,Input  } from '@angular/core';
+import { Component, inject, signal, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+//src\app\shared\interfaces\models\banner.model.ts
 @Component({
   selector: 'app-headers',
   imports: [CommonModule],
@@ -8,5 +8,7 @@ import { CommonModule } from '@angular/common';
   styleUrl: './headers.scss',
 })
 export class HeadersComponent  {
+  @Input() characterImage: string = '';
   @Input() title: string = 'Título por defecto';
+
 }
