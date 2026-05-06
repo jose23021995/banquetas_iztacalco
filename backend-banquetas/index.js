efd1588a08889556447662a7be91a66ce9162751
jose23021995 <80795569+jose23021995@users.noreply.github.com>
2026-05-06 09:29:19 -0600

    visualizacion de imagenes

--- backend-banquetas/index.js
+++ backend-banquetas/index.js
@@ -6,11 +6,11 @@ const authRoutes = require('./routes/auth.routes');
 const jwt = require('jsonwebtoken');
 
 const app = express();
-
+const path = require('path'); // Importante importar path
 // Middlewares
 app.use(cors());
 app.use(express.json());
-
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 // Rutas
 app.use('/api/catalogos', catalogosRoutes);
 app.use('/api/auth', authRoutes);
